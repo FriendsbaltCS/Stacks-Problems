ebfcabdcd1b6a2a2426da2adaf458a2f7089fbc8
Joel <jhammer@friendsbalt.org>
2024-11-04 20:24:45 -0500

    Use built-in float conversion

--- dijkstra.py
+++ dijkstra.py
@@ -1,7 +1,7 @@
 def evaluate_expression(s):
     pass
 
-def parse_numerical_value(exp, index = 0):
+def parse_numerical_value(exp, index=0):
     """
     Parses a numerical value from a string expression starting at the given index.
     Args:
@@ -11,26 +11,12 @@ def parse_numerical_value(exp, index = 0):
         tuple: A tuple containing the parsed numerical value (float) and the index (int) 
                 where the parsing ended.
     """
-    is_neg = False
+    start_index = index
     if exp[index] == "-":
-        is_neg = True
         index += 1
 
-    value = 0
-    decimal_place = -1
     while index < len(exp) and (exp[index].isdigit() or exp[index] == '.'):
-        if exp[index] == '.':
-            decimal_place = 0
-        else:
-            digit = int(exp[index])
-            if decimal_place == -1:
-                value = value * 10 + digit
-            else:
-                decimal_place += 1
-                value += digit * (10 ** -decimal_place)
         index += 1
 
-    if is_neg:
-        value = -value
-
+    value = float(exp[start_index:index])
     return value, index
