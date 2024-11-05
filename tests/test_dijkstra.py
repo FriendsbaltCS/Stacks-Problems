32e3bcf867189626e03f34ef3e7f85b04007a50b
Joel <jhammer@friendsbalt.org>
2024-11-04 21:24:49 -0500

    Expand dijkstra tests

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -33,6 +33,18 @@ def test_expressions_with_parentheses():
     assert evaluate_expression("2*(3+4)") == 14
     assert evaluate_expression("(2+3)*(4-1)") == 15
 
+def test_expressions_with_spaces():
+    assert evaluate_expression(" 3 + 2 ") == 5
+    assert evaluate_expression(" 10 - 3 ") == 7
+    assert evaluate_expression(" 4 * 5 ") == 20
+    assert evaluate_expression(" 8 / 2 ") == 4
+
+def test_equal_precedence_operations():
+    assert evaluate_expression("3+3+3") == 9
+    assert evaluate_expression("10-3+2") == 9
+    assert evaluate_expression("4*5*2") == 40
+    assert evaluate_expression("8/2/2") == 2
+
 def test_edge_cases():
     assert evaluate_expression("") == 0
     assert evaluate_expression("42") == 42
